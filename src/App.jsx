9a9026b91c66055be8ce2a792924387b70d5790b
Thet Htoo Zaw <thethtoozaw@blue-stone.net>
2025-06-30 15:56:58 +0630

    [Done] React-Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
-function Item({ content, name }){
-  return(
-    <li style={{ padding: 10, borderBottom: "1px solid #ddd"}}>
-      {content} -<b>{name}</b>
-    </li>
-  );
-}
+import { useState } from "react";
+import Item from "./Item";
+import List from "./List";
+import Form from "./Form";
 
-function List({ children }){
-  return <ul style={{
-    listStyle : "none",
-    padding : 0,
-    margin: 0,
-    border: "1px solid #ddd",
-    borderRadius: 8,
-    overflow: "hidden", 
-  }}>
-    {children}
-  </ul>;
-}
 
 export default function App() {
+
+  const [showForm, setShowForm] = useState(false);
+
+  const [data,setData] = useState([
+    {id: 1, content: "Hello, World!", name: "Alice"},
+    {id: 2, content: "React is fun!", name: "Bob"},
+    {id: 3, content: "Ha Ha Ha..", name: "James"}
+  ]);
+
+  const remove = id => {
+    setData(data.filter(item => item.id !== id));
+  };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, {id, content, name}])
+  }
+
   return (
-    <div style={{ maxWidth: 600, margin: "2px auto" }}>
-      <h1>Yaycha</h1>
+    <div style={{ maxWidth: 600, margin: "20px auto" }}>
+      <h1 style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        Yaycha
+        <button
+          onClick={() => setShowForm(!showForm)}
+          style={{
+            width: 32,
+            height: 32,
+            borderRadius: 50,
+            border: "0 none",
+            background: showForm ? "#dc3545" : "#0d6efd" ,
+            color: "white"
+          }}
+        >
+          {showForm ? "x" : "+"}
+        </button>
+        </h1>
+      {showForm && <Form add={add}/> }
       <List>
-        <Item content="Hello, World!" name="Alice" />
-        <Item content="React is fun!" name="Bob" />
+        {data.map(item => (
+          <Item key={item.id} item={item} remove={remove} />
+        ))}
       </List>
     </div>
   );
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,7 +1,16 @@
-export default function Item({ content, name }){
+export default function Item({ item, remove }){
     return (
-        <li>
-            {content} - <b>{name}</b>
+        <li style={{ 
+            padding: 10, 
+            borderBottom: "1px solid #333",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between"
+            }}>
+            <span>
+                {item.content} - <b>{item.name}</b>
+            </span>
+            <button onClick={() => remove(item.id)}>Delete</button>
         </li>
     );
 }
